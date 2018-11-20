b407f1b5960b73d80f30c99152f9fa10cf03811b
drafire <hyljljlj@163.com>
2018-11-20 08:32:40 +0800

    初始化了自定义协议的命令枚举及自定义消息实体的私有域（变量）

--- src/main/java/com/drafire/protocol/IMMessage.java
+++ src/main/java/com/drafire/protocol/IMMessage.java
@@ -1,7 +1,77 @@
 package com.drafire.protocol;
 
+import org.msgpack.annotation.Message;
+
 /**
  * 自定义消息实体
+ * 加上@Message，用于msgpack序列化
  */
+@Message
 public class IMMessage {
+    private String addr;        //ip地址+端口
+    private String cmd;         //命令
+    private long time;         //系统时间戳
+    private int online;        //在线人数
+    private String sender;     //发送者
+    private String receiver;   //接收人
+    private String content;    //内容
+
+    public IMMessage() {
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getCmd() {
+        return cmd;
+    }
+
+    public void setCmd(String cmd) {
+        this.cmd = cmd;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public int getOnline() {
+        return online;
+    }
+
+    public void setOnline(int online) {
+        this.online = online;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
--- src/main/java/com/drafire/protocol/IMP.java
+++ src/main/java/com/drafire/protocol/IMP.java
@@ -5,4 +5,32 @@ package com.drafire.protocol;
  *
  */
 public enum IMP {
+    /** 系统消息 */
+    SYSTEM("SYSTEM"),
+    /** 登录指令 */
+    LOGIN("LOGIN"),
+    /** 登出指令 */
+    LOGOUT("LOGOUT"),
+    /** 聊天消息 */
+    CHAT("CHAT"),
+    /** 送鲜花 */
+    FLOWER("FLOWER");
+
+    private String name;
+
+    public static boolean isIMP(String content){
+        return content.matches("^\\[(SYSTEM|LOGIN|LOGIN|CHAT)\\]");
+    }
+
+    IMP(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String toString(){
+        return this.name;
+    }
 }
