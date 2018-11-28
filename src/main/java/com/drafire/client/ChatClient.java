7fbf6e14d928fe0d88c4e24ee8634b7245b0c6e7
pangtinghuan <pangtinghuan@tuandai.com>
2018-11-28 13:16:27 +0800

    初步实现了客户端的逻辑

--- src/main/java/com/drafire/client/ChatClient.java
+++ src/main/java/com/drafire/client/ChatClient.java
@@ -1,7 +1,56 @@
 package com.drafire.client;
 
+import com.drafire.client.handler.ChatClientHandler;
+import com.drafire.protocol.IMDecoder;
+import com.drafire.protocol.IMEncoder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 /**
  * 客户端
  */
 public class ChatClient {
+    private ChatClientHandler handler;
+    private String host;
+    private int port;
+
+    public ChatClient(String nickName) {
+        this.handler = new ChatClientHandler(nickName);
+    }
+
+    public void connect(String host,int port) {
+        this.host=host;
+        this.port=port;
+
+        EventLoopGroup workerGroup=new NioEventLoopGroup();
+
+        Bootstrap b=new Bootstrap();
+        try {
+            b.group(workerGroup).channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new IMDecoder());
+                            socketChannel.pipeline().addLast(new IMEncoder());
+                            socketChannel.pipeline().addLast(handler);
+                        }
+                    });
+
+            ChannelFuture f=b.connect(this.host,this.port).sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatClient("drafire").connect("127.0.0.1",8083);
+    }
 }
--- src/main/java/com/drafire/client/handler/ChatClientHandler.java
+++ src/main/java/com/drafire/client/handler/ChatClientHandler.java
@@ -1,7 +1,84 @@
 package com.drafire.client.handler;
 
+import com.drafire.protocol.IMMessage;
+import com.drafire.protocol.IMP;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.apache.log4j.Logger;
+
+import java.util.Scanner;
+
 /**
  * 客户端api类
  */
-public class ChatClientHandler {
+public class ChatClientHandler extends ChannelInboundHandlerAdapter {
+    private static Logger logger = Logger.getLogger(ChatClientHandler.class);
+    private ChannelHandlerContext context;
+    private String nickName;
+
+    public ChatClientHandler(String nickName) {
+        this.nickName = nickName;
+    }
+
+    /**
+     * 连接上了服务器后回调
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        this.context = ctx;
+        IMMessage message = new IMMessage(IMP.LOGIN.getName(), System.currentTimeMillis(), this.nickName);
+        sendMessage(message);
+        logger.info("成功连接服务器，并执行了登录动作");
+        session();
+    }
+
+    private boolean sendMessage(IMMessage msg) {
+        context.channel().writeAndFlush(msg);
+        logger.info("已发送至聊天面板,请继续输入");
+        return msg.getCmd().equals(IMP.LOGOUT) ? false : true;
+    }
+
+    private void session() {
+        new Thread() {
+            public void run() {
+                logger.info(nickName + ",你好，请在控制台输入消息内容");
+                IMMessage message = null;
+                Scanner scanner = new Scanner(System.in);
+
+                do {
+                    if (scanner.hasNext()) {
+                        String input = scanner.nextLine();
+                        if ("exist".equals(input)) {
+                            message = new IMMessage(IMP.LOGOUT.getName(), System.currentTimeMillis(), nickName);
+                        } else {
+                            message = new IMMessage(IMP.CHAT.getName(), System.currentTimeMillis(), nickName, input);
+                        }
+                    }
+                } while (sendMessage(message));
+                scanner.close();
+            }
+        }.start();
+    }
+
+    /**
+     * 收到消息后回调
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        IMMessage message = (IMMessage) msg;
+        logger.info(message);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.error(cause.getMessage());
+        ctx.close();
+    }
 }
